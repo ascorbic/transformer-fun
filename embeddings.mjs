ddab0bace8a652ee59532defdec6ff9e348b4626
Matt Kane <m@mk.gg>
2023-07-14 22:56:42 +0100

    Multi value

--- embeddings.mjs
+++ embeddings.mjs
@@ -1,6 +1,7 @@
 import os from "node:os";
 import { join } from "node:path";
 import { pipeline } from "@xenova/transformers";
+
 const extractor = await pipeline(
   "feature-extraction",
   "Xenova/multi-qa-MiniLM-L6-cos-v1",
@@ -9,22 +10,36 @@ const extractor = await pipeline(
   }
 );
 export async function handler(event, context) {
-  console.log("tmp", os.tmpdir());
+  const texts = [];
   try {
-    const { text } = event.queryStringParameters;
-    if (!text) {
+    if (event.httpMethod === "GET") {
+      texts.push(event.queryStringParameters.text);
+    } else if (event.httpMethod === "POST") {
+      const body = JSON.parse(event.body);
+      texts.push(...body.texts);
+    }
+    if (texts.length === 0) {
       return {
         statusCode: 400,
         body: "Please provide text to embed",
       };
     }
+
     console.time("embed");
-    const result = await extractor(text, { pooling: "mean", normalize: true });
+    const results = await Promise.all(
+      texts.map(async (text) => {
+        const { data } = await extractor(text, {
+          pooling: "mean",
+          normalize: true,
+        });
+        return data;
+      })
+    );
     console.timeEnd("embed");
 
     return {
       statusCode: 200,
-      body: JSON.stringify(result),
+      body: JSON.stringify(results),
       headers: {
         "Content-Type": "application/json",
       },
